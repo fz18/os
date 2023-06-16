c068666c75d588cc92213fc33eb30d1e1da8d1b9
fengzhao <2399127626@qq.com>
2023-06-16 17:14:09 +0800

    simple shell

--- kernel/main.c
+++ kernel/main.c
@@ -10,20 +10,18 @@
 #include "memory.h"
 #include "fs.h"
 #include "dir.h"
+#include "assert.h"
+#include "shell.h"
 
 void init(void);
-void k_thread_a(void *);
-void k_thread_b(void *);
-void u_prog_a(void);
-void u_prog_b(void);
 
 int main(void)
 {
    put_str("I am kernel\n");
    init_all();
-   intr_enable();
-
-     while (1)
+   cls_screen();
+   console_put_str("[rabbit@localhost /]$ ");
+   while (1)
       ;
    return 0;
 }
@@ -33,97 +31,13 @@ void init(void)
 {
    uint32_t ret_pid = fork();
    if (ret_pid)
-   {
-      printf("i am father, my pid is %d, child pid is %d\n", getpid(), ret_pid);
+   { // 父进程
+      while (1)
+         ;
    }
    else
-   {
-      printf("i am child, my pid is %d, ret pid is %d\n", getpid(), ret_pid);
+   { // 子进程
+      my_shell();
    }
-   while (1)
-      ;
-}
-
-/* 在线程中运行的函数 */
-void k_thread_a(void *arg)
-{
-   void *addr1 = sys_malloc(256);
-   void *addr2 = sys_malloc(255);
-   void *addr3 = sys_malloc(254);
-   console_put_str(" thread_a malloc addr:0x");
-   console_put_int((int)addr1);
-   console_put_char(',');
-   console_put_int((int)addr2);
-   console_put_char(',');
-   console_put_int((int)addr3);
-   console_put_char('\n');
-
-   int cpu_delay = 100000;
-   while (cpu_delay-- > 0)
-      ;
-   sys_free(addr1);
-   sys_free(addr2);
-   sys_free(addr3);
-   while (1)
-      ;
-}
-
-/* 在线程中运行的函数 */
-void k_thread_b(void *arg)
-{
-   void *addr1 = sys_malloc(256);
-   void *addr2 = sys_malloc(255);
-   void *addr3 = sys_malloc(254);
-   console_put_str(" thread_b malloc addr:0x");
-   console_put_int((int)addr1);
-   console_put_char(',');
-   console_put_int((int)addr2);
-   console_put_char(',');
-   console_put_int((int)addr3);
-   console_put_char('\n');
-
-   int cpu_delay = 100000;
-   while (cpu_delay-- > 0)
-      ;
-   sys_free(addr1);
-   sys_free(addr2);
-   sys_free(addr3);
-   while (1)
-      ;
-}
-
-/* 测试用户进程 */
-void u_prog_a(void)
-{
-   void *addr1 = malloc(256);
-   void *addr2 = malloc(255);
-   void *addr3 = malloc(254);
-   printf(" prog_a malloc addr:0x%x,0x%x,0x%x\n", (int)addr1, (int)addr2, (int)addr3);
-
-   int cpu_delay = 100000;
-   while (cpu_delay-- > 0)
-      ;
-   free(addr1);
-   free(addr2);
-   free(addr3);
-   while (1)
-      ;
-}
-
-/* 测试用户进程 */
-void u_prog_b(void)
-{
-   void *addr1 = malloc(256);
-   void *addr2 = malloc(255);
-   void *addr3 = malloc(254);
-   printf(" prog_b malloc addr:0x%x,0x%x,0x%x\n", (int)addr1, (int)addr2, (int)addr3);
-
-   int cpu_delay = 100000;
-   while (cpu_delay-- > 0)
-      ;
-   free(addr1);
-   free(addr2);
-   free(addr3);
-   while (1)
-      ;
+   panic("init: should not be here");
 }
